81da102111edd24f354be91b26e421b5c4f95a3d
Alexandr_Zhukov <sh.zhukov@gmail.com>
2021-03-06 18:59:18 +0300

    первая проба

--- src/ru/geekbrains/test1/Main.java
+++ src/ru/geekbrains/test1/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello World");
+        SecondClass s1 = new SecondClass();
+        s1.print();
     }
+
 }
---
+++ src/ru/geekbrains/test1/SecondClass.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.test1;
+
+public class SecondClass {
+
+    public SecondClass() {
+    }
+
+    public void print() {
+        System.out.println("print");
+    }
+}
